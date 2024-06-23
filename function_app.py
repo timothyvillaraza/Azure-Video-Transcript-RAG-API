da1c50317b67020ff625a66463fe8cfd9f5e7d7e
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-06-23 13:52:30 -0500

    CI/CD Test 2

--- function_app.py
+++ function_app.py
@@ -12,7 +12,7 @@ app.register_blueprint(blueprint_service_test)
 def first_http_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            "CI/CD TEST: This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
             status_code=200
     )
 

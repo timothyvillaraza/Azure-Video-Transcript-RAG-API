80939d727326b8a39012c5b253ae3c68eff087e4
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-08-12 18:35:43 -0500

    Uncommented  blueprint functions

--- function_app.py
+++ function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 import logging
 import csv
 import codecs
-from services.example_service.api.functions.myfunction import bp as blueprint_service_test_functions
+# from services.example_service.api.functions.myfunction import bp as blueprint_service_test_functions
 from services.video_rag.api.functions.video_rag_functions import bp as blueprint_video_rag_functions
 from services.video_rag.api.functions.chat_message_functions import bp as blueprint_chat_message_functions
 from services.video_rag.api.functions.session_functions import bp as blueprint_session_functions
@@ -14,5 +14,5 @@ app = func.FunctionApp()
 # app.register_blueprint(blueprint_service_test_functions)
 app.register_blueprint(blueprint_chat_message_functions)
 app.register_blueprint(blueprint_video_rag_functions)
-# app.register_blueprint(blueprint_session_functions)
-# app.register_blueprint(blueprint_video_functions)
+app.register_blueprint(blueprint_session_functions)
+app.register_blueprint(blueprint_video_functions)

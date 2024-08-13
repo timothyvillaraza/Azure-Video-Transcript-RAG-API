491c2e651eb7529846cacceb5d5742a40c8886cc
timothyvillaraza <61322637+timothyvillaraza@users.noreply.github.com>
2024-08-12 19:57:06 -0500

    pipeline update

--- function_app.py
+++ function_app.py
@@ -1,8 +1,5 @@
 import azure.functions as func
-import logging
-import csv
-import codecs
-# from services.example_service.api.functions.myfunction import bp as blueprint_service_test_functions
+from services.example_service.api.functions.myfunction import bp as blueprint_service_test_functions
 from services.video_rag.api.functions.video_rag_functions import bp as blueprint_video_rag_functions
 from services.video_rag.api.functions.chat_message_functions import bp as blueprint_chat_message_functions
 from services.video_rag.api.functions.session_functions import bp as blueprint_session_functions
